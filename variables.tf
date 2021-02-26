198b8dcdc48d96651ed6c2558b5f6abd9bfcc74d
clam5917 <71423588+clam5917@users.noreply.github.com>
2021-02-25 16:59:27 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,5 +3,5 @@ variable "web_count" {}
 variable "subnet_id" {}
 variable "identity" {}
 variable "vpc_security_group_ids" {
-  type = ["sg-02713b4780094ac55"]
+  value = ["sg-02713b4780094ac55"]
 }
